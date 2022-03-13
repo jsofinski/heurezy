77504160e8d20c2bf9c291f324ba400540a4bf5d
Mateusz Kmak <hamsterkmak@gmail.com>
2022-03-13 18:33:35 +0100

    fixed random tsp generating

--- randomtsp.jl
+++ randomtsp.jl
@@ -4,53 +4,50 @@ using Printf;
 
 function genAsymmetricalRandomTsp(numOfNodes::Int, weightLowerBound::Int, weightUpperBound::Int)
   tsp = readTSPLIB(:gr17)
-  tsp.name = @sprintf("asymmetrical%d-%d-%d", numOfNodes, weightLowerBound, weightUpperBound)
-  tsp.dimension = numOfNodes
-  tsp.weights = zeros(numOfNodes, numOfNodes)
-  tsp.optimal = 0.0
-  for i in 0:numOfNodes
-    for j in 0:numOfNodes
-      tsp.weights[i][j] = rand(weightLowerBound:weightUpperBound)
+  name = @sprintf("asymmetrical%d-%d-%d", numOfNodes, weightLowerBound, weightUpperBound)
+  dimension = numOfNodes
+  weights = zeros(numOfNodes, numOfNodes)
+  for i in 1:numOfNodes
+    for j in 1:numOfNodes
+      weights[i, j] = rand(weightLowerBound:weightUpperBound)
     end
   end
-  return tsp
+	return TSP(name, dimension, tsp.weight_type, weights, tsp.nodes, tsp.Dnodes, tsp.ffx, tsp.pfx, 0.0)
 end
 
 function genSymmetricalRandomTsp(numOfNodes::Int, weightLowerBound::Int, weightUpperBound::Int)
   tsp = readTSPLIB(:gr17)
-  tsp.name = @sprintf("symmetrical%d-%d-%d", numOfNodes, weightLowerBound, weightUpperBound)
-  tsp.dimension = numOfNodes
-  tsp.weights = zeros(numOfNodes, numOfNodes)
-  tsp.optimal = 0.0
-  for i in 0:(numOfNodes+1)/2
-    for j in 0:(numOfNodes+1)/2
+  name = @sprintf("symmetrical%d-%d-%d", numOfNodes, weightLowerBound, weightUpperBound)
+  dimension = numOfNodes
+  weights = zeros(numOfNodes, numOfNodes)
+	for i in 1:numOfNodes
+		for j in i:numOfNodes
       dist = rand(weightLowerBound:weightUpperBound)
-      tsp.weights[i][j] = dist
-      tsp.weights[j][i] = dist
+      weights[i, j] = dist
+      weights[j, i] = dist
     end
   end
-  return tsp
+	return TSP(name, dimension, tsp.weight_type, weights, tsp.nodes, tsp.Dnodes, tsp.ffx, tsp.pfx, 0.0)
 end
 
 function genEuclidianRandomTsp(numOfNodes::Int, maxX::Int, maxY::Int)
   tsp = readTSPLIB(:berlin52)
-  tsp.name = @sprintf("euclidian%d-%d-%d", numOfNodes, maxX, maxY)
-  tsp.dimension = numOfNodes
-  tsp.weights = zeros(numOfNodes, numOfNodes)
-  tsp.optimal = 0.0
-  tsp.nodes = zeros(numOfNodes, 2)
-  for i in 0:numOfNodes
-    tsp.nodes[i, 0] = rand(0:maxX)
-		tsp.nodes[i, 1] = rand(0:maxY)
+  name = @sprintf("euclidian%d-%d-%d", numOfNodes, maxX, maxY)
+  dimension = numOfNodes
+  weights = zeros(numOfNodes, numOfNodes)
+  nodes = zeros(numOfNodes, 2)
+  for i in 1:numOfNodes
+    nodes[i, 1] = rand(0:maxX)
+		nodes[i, 2] = rand(0:maxY)
   end
-  for i in 0:numOfNodes
-    for j in 0:numOfNodes
-			tsp.weights[i][j] = round(
+  for i in 1:numOfNodes
+    for j in 1:numOfNodes
+			weights[i, j] = round(
 				sqrt(
-					(tsp.nodes[i, 0] - tsp.nodes[j, 0])^2 + (tsp.nodes[i, 1] - tsp.nodes[j, 1])^2
+					(nodes[i, 1] - nodes[j, 1])^2 + (nodes[i, 2] - nodes[j, 2])^2
 				), RoundNearest
 			)
     end
   end
-  return tsp
+	return TSP(name, dimension, tsp.weight_type, weights, nodes, tsp.Dnodes, tsp.ffx, tsp.pfx, 0.0)
 end
