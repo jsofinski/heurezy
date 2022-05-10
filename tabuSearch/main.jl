e0ba788124f805423e957beb36776e94d6940ef3
Mateusz Kmak <hamsterkmak@gmail.com>
2022-05-10 23:18:37 +0200

    long term memory works

--- tabuSearch/main.jl
+++ tabuSearch/main.jl
@@ -7,46 +7,76 @@ using TSPLIB
 using Random
 using Pkg
 
-function tabuSearch(tsp, maxIterations, baseFunction, tabuSize)
-  tenure = Queue{Tuple{Int, Int}}()
-  tenureSet = Set{Tuple{Int, Int}}()
+struct LongTermMemoryObject
+  path::Vector{Int64}
+  localBestPath::Vector{Int64}
+  localBestWeight::Float64
+end
+
+function tabuSearch(tsp, maxIterations, baseFunction, tabuSize, allIterationsMax)
+	println("starting tabu search")
+  tenure = Queue{Tuple{Int,Int}}()
+  tenureSet = Set{Tuple{Int,Int}}()
+  longTermMemory = Queue{LongTermMemoryObject}()
   path = baseFunction(tsp)
   bestPath = path
+  globalBestPath = path
+  nextPath = path
+  allIterations = 0
 
   # println(objectiveFunction(tsp, bestPath))
   # println(bestPath)
 
+  bestWeight = objectiveFunction(tsp, path) #local best
+  globalBestWeight = bestWeight
 
-  bestWeight = objectiveFunction(tsp, path) #global best
   # currentWeight = objectiveFunction(tsp, path) #iteration variable
   stopCondition = false
   iterationsWithoutImprovement = 0
 
 
   while !stopCondition
+    allIterations += 1
     beforeWeight = bestWeight #best weight at the beggining of itetarion
     # if tenureSize is smaller than maxIterations it might not get out of the local minimum
-    neighbourhood = allInverts(tsp, bestPath)
+    neighbourhood = allInverts(tsp, nextPath)
     bestCandidate = neighbourhood[1]
     # if neighbourhood[1] is in tenure
     if ((neighbourhood[1].from, neighbourhood[1].to) in tenureSet)
       bestCandidate = InvertResult(neighbourhood[1].from, neighbourhood[1].to, typemax(Float64))
     end
-    
+
     for i in 2:length(neighbourhood)
-      if neighbourhood[i].fitness < bestCandidate.fitness && !((neighbourhood[i].from, neighbourhood[i].to) in tenureSet)
-        bestCandidate = neighbourhood[i]
+      if !((neighbourhood[i].from, neighbourhood[i].to) in tenureSet)
+        if neighbourhood[i].fitness < bestCandidate.fitness
+          newRecord = LongTermMemoryObject(
+            invert(deepcopy(nextPath), neighbourhood[i].from, neighbourhood[i].to),
+            deepcopy(bestPath),
+            bestWeight)
+          enqueue!(longTermMemory, newRecord)
+        end
+        if neighbourhood[i].fitness < bestCandidate.fitness
+          bestCandidate = neighbourhood[i]
+        end
       end
     end
 
-    tuple = (bestCandidate.from, bestCandidate.to) 
+    tuple = (bestCandidate.from, bestCandidate.to)
     # tuple = Tuple(bestCandidate.from, bestCandidate.to) 
 
     # Check if got any improvement
     # Set iteration best 
+    nextPath = invert(nextPath, bestCandidate.from, bestCandidate.to)
     if (bestCandidate.fitness < bestWeight)
       bestWeight = bestCandidate.fitness
+      bestPath = nextPath
       iterationsWithoutImprovement = 0
+      if bestWeight < globalBestWeight
+        globalBestWeight = bestWeight
+				globalBestPath = deepcopy(nextPath)
+      end
+    else
+      iterationsWithoutImprovement += 1
     end
 
     push!(tenureSet, tuple)
@@ -55,17 +85,27 @@ function tabuSearch(tsp, maxIterations, baseFunction, tabuSize)
       lastTuple = dequeue!(tenure)
       delete!(tenureSet, lastTuple)
     end
-    # step closer to stop condition 
-    if beforeWeight >= bestCandidate.fitness
-      iterationsWithoutImprovement += 1
-    end
+
+
     # check stopCondition
     if iterationsWithoutImprovement >= maxIterations
+      if !isempty(longTermMemory)
+        previousRecord = dequeue!(longTermMemory)
+        nextPath = previousRecord.path
+        bestPath = previousRecord.localBestPath
+        bestWeight = previousRecord.localBestWeight
+      else
+        stopCondition = true
+      end
+    end
+
+    if allIterations >= allIterationsMax
       stopCondition = true
     end
+
   end
-  println(objectiveFunction(tsp, bestPath))
-  println(bestPath)
+  println(objectiveFunction(tsp, globalBestPath))
+  println(globalBestPath)
 end
 
 
--- tabuSearch/test.jl
+++ tabuSearch/test.jl
@@ -1,12 +1,22 @@
 include("main.jl")
 
+function getKrandom(k)
+
+  function krandomSpecified(tsp)
+    krandom(tsp, k)
+  end
+
+  return krandomSpecified
+end
+
 function allInvertTest()
   tsp = readTSPLIB(:gr24)
   display(allInverts(tsp, [1,2,3, 4, 5, 6, 7, 8]))
 end
 function test() 
     tsp = readTSPLIB(:gr24)
-    tabuSearch(tsp,1000,closestNeighbourAlgorithm, 2000)
+	tabuSearch(tsp,1000,getKrandom(2), 200, 1000)
+	println(tsp.optimal)
 end
 
 
