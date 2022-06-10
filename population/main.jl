676fed904e6d78c44dcf874f15867ac709dfaf27
jsofinski <kubsof@gmail.com>
2022-06-10 19:43:07 +0200

    many island handled without mixing

--- population/main.jl
+++ population/main.jl
@@ -10,11 +10,11 @@ using TSPLIB
 # The most basic one is a random population generator.
 # memify - function (tsp, path) -> path, takes the path and tries to make it better. The most basic case is an identity function.
 # maxIterations - stop condition
-function populationSearch(tsp, islandGetters, memify, maxIterations)
+function populationSearch(tsp, islandGetters, memify, maxIterations, populationSize)
   iteration = 0
   islands = Array{Array{Int}}[]
   for i in 1:length(islandGetters)
-		push!(islands, islandGetters[i]())
+		push!(islands, islandGetters[i](populationSize))
   end
 
 
@@ -34,61 +34,76 @@ function populationSearch(tsp, islandGetters, memify, maxIterations)
   println(bestFitness)
   println(currentIsland[bestIndex])
 
+  NOIslands = length(islands)
   while true 
     # some crossover
 
-    tempPath = orderedCrossover(currentIsland[1], currentIsland[2])
+
+    # tempchildren = partiallyMappedCrossover(currentIsland[1], currentIsland[2])
+    # tempchildren = partiallyMappedCrossover([1,2,3,4,5,6,7,8,9], [3,1,6,2,8,7,4,5,9])
     # println(currentIsland[1])
     # println(currentIsland[2])
-    # println(tempPath[1])
-    # println(tempPath[2])
-
-    size = length(currentIsland)
-    for i in 1:size 
-      randFirst = rand(1:size)
-      randSecond = rand(1:size)
-      tempchildren = orderedCrossover(currentIsland[randFirst], currentIsland[randSecond])
-      
-
-      # mutacja
-      randFirst = rand(1:length(tempchildren[1]))
-      randSecond = rand(1:length(tempchildren[1]))
-      tempchildren[1] = swapNodesInPath(tempchildren[1], randFirst, randSecond)
-      tempchildren[2] = swapNodesInPath(tempchildren[2], randFirst, randSecond)
-
-      push!(currentIsland, tempchildren[1])
-      push!(currentIsland, tempchildren[2])
-    end
+    # println(tempchildren[1])
+    # println(tempchildren[2])
+    # break
     
+    for i in 1:NOIslands
+      currentIsland = islands[i]
+      size = length(currentIsland)
+      for j in 1:size 
+        randFirst = rand(1:size)
+        randSecond = rand(1:size)
+        # tempchildren = partiallyMappedCrossover(currentIsland[randFirst], currentIsland[randSecond])
+        tempchildren = orderedCrossover(currentIsland[randFirst], currentIsland[randSecond])
+
+        # mutacja
+        randFirst = rand(1:length(tempchildren[1]))
+        randSecond = rand(1:length(tempchildren[1]))
+        tempchildren[1] = swapNodesInPath(tempchildren[1], randFirst, randSecond)
+        tempchildren[2] = swapNodesInPath(tempchildren[2], randFirst, randSecond)
+        # println(tempchildren[1])
+        push!(currentIsland, tempchildren[1])
+        push!(currentIsland, tempchildren[2])
+      end
+    end
     # memify child
     
 
     # check maxIterations
     iteration += 1
+    
     if iteration >= maxIterations
       println("koniec, tutej mozna wybrac najlepsza sciezke elo")
-      bestFitness = objectiveFunction(tsp, currentIsland[1])
-      bestIndex = 1
-      for i in 2:length(currentIsland)
-        println(objectiveFunction(tsp, currentIsland[i]))
-        if objectiveFunction(tsp, currentIsland[i]) < bestFitness
-          bestIndex = i
-          bestFitness = objectiveFunction(tsp, currentIsland[i])
+      for i in 1:NOIslands
+        currentIsland = islands[i]
+        bestFitness = objectiveFunction(tsp, currentIsland[1])
+        bestIndex = 1
+        currentIsland = islands[i]
+        for i in 2:length(currentIsland)
+          if objectiveFunction(tsp, currentIsland[i]) < bestFitness
+            bestIndex = i
+            bestFitness = objectiveFunction(tsp, currentIsland[i])
+          end
         end
+        println("Island: ", i)
+        println(bestFitness)
+        println(currentIsland[bestIndex])
       end
-      println(bestFitness)
-      println(currentIsland[bestIndex])
-      break
+    break
+
     end
 
     # selection -> tournament: get random 2 paths and remove worse, should be faster than sorting, idk
-    while length(currentIsland) > size 
-      randFirst = rand(1:length(currentIsland))
-      randSecond = rand(1:length(currentIsland))
-      if objectiveFunction(tsp, currentIsland[randFirst]) < objectiveFunction(tsp, currentIsland[randSecond])
-        deleteat!(currentIsland, randSecond)
-      else 
-        deleteat!(currentIsland, randFirst)
+    for i in 1:NOIslands
+      currentIsland = islands[i]
+      while (length(currentIsland) > populationSize)
+        randFirst = rand(1:populationSize)
+        randSecond = rand(1:populationSize)
+        if objectiveFunction(tsp, currentIsland[randFirst]) < objectiveFunction(tsp, currentIsland[randSecond])
+          deleteat!(currentIsland, randSecond)
+        else 
+          deleteat!(currentIsland, randFirst)
+        end
       end
     end
 
@@ -148,6 +163,52 @@ function orderedCrossover(firstParent, secondParent)
       end 
     end 
     secondChild[i+1] = firstParent[j]
+  end
+
+  return [firstChild, secondChild]
+end
+
+function partiallyMappedCrossover(firstParent, secondParent)
+  size = length(firstParent)
+  firstChild = zeros(Int, size, 1)
+  secondChild = zeros(Int, size, 1)
+
+
+  randPos = Int(floor(rand(size/3:(size-size/3))))
+  firstMap = zeros(Int, randPos, 1)
+  secondMap = zeros(Int, randPos, 1)
+  # println(randPos)
+  
+  for i in 1:randPos
+    firstChild[i] = firstParent[i]
+    secondChild[i] = secondParent[i]
+    firstMap[i] = firstParent[i]
+    secondMap[i] = secondParent[i]
+  end
+
+  for i in randPos+1:size
+    temp = secondParent[i]
+    while temp in firstMap
+      for j in 1:randPos
+        if temp == firstMap[j]
+          temp = secondMap[j]
+          break
+        end
+      end
+    end
+    firstChild[i] = temp
+
+    temp = firstParent[i]
+    while temp in secondMap
+      for j in 1:randPos
+        if temp == secondMap[j]
+          temp = firstMap[j]
+          break
+        end
+      end
+    end
+    secondChild[i] = temp
+
   end
 
   return [firstChild, secondChild]
--- population/test.jl
+++ population/test.jl
@@ -5,17 +5,17 @@ include("main.jl")
 
 using TSPLIB
 
-function getArray(size) 
+function getArray(NOislands) 
     getters = []
-    for i in 1:size
+    for i in 1:NOislands
         push!(getters, funcGetPopulation)
     end
     return getters
 end
 
-function funcGetPopulation()
+function funcGetPopulation(size)
     tsp = readTSPLIB(:gr24)
-    return getPopulation(tsp, 1000)
+    return getPopulation(tsp, size)
 end
 
 function getPopulation(tsp, sizeOfPopulation) 
@@ -35,6 +35,6 @@ end
 tsp = readTSPLIB(:gr24)
 getPopulation(tsp, 10)
 
-populationSearch(tsp, getArray(1), test, 1000)
+populationSearch(tsp, getArray(3), test, 100, 1000)
 println("optimal:")
 println(TSPLIB.Optimals[:gr24])
